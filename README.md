ffd82c19cf407f58a0d259e692fdc51db315af17
Jakub Ostrowski <cespenar1@gmail.com>
2022-04-06 12:48:43 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# sdBGridViewer
-=================
+# sdBGridViever
+
+***
+
+## Overview
+The tool to quickly inspect the grid of evolutionary models of sdB stars
+calculated for the [ARDASTELLA](https://ardastella.up.krakow.pl/) research
+group.
+
+The models were calculated using the set-up described
+by [Ostrowski et al. (2021)](https://ui.adsabs.harvard.edu/abs/2021MNRAS.503.4646O/abstract)
+, using the computational resources provided
+by [Wrocław Centre for Networking and Supercomputing](https://www.wcss.pl/en/).
+Evolutionary models were calculated using the
+[MESA](https://github.com/MESAHub/mesa) code
+([Paxton et al. 2011](https://ui.adsabs.harvard.edu/abs/2011ApJS..192....3P/abstract),
+[2013](https://ui.adsabs.harvard.edu/abs/2013ApJS..208....4P/abstract), 
+[2015](https://ui.adsabs.harvard.edu/abs/2015ApJS..220...15P/abstract), 
+[2018](https://ui.adsabs.harvard.edu/abs/2018ApJS..234...34P/abstract), 
+[2019](https://ui.adsabs.harvard.edu/abs/2019ApJS..243...10P/abstract)),
+version 11701, and supplemented by adiabatic non-radial pulsational models
+calculated using the [GYRE](https://github.com/rhdtownsend/gyre) code
+([Townsend & Teitler 2013](https://ui.adsabs.harvard.edu/abs/2013MNRAS.435.3406T/abstract),
+[Townsend et al. 2018](https://ui.adsabs.harvard.edu/abs/2018MNRAS.475..879T/abstract),
+[Goldstein & Townsend 2020](https://ui.adsabs.harvard.edu/abs/2020ApJ...899..116G/abstract)),
+version 5.2.
+
+## Available plots
+There are three predefined plots available: $$\log\,g$$ vs. $$T_\mathrm{eff)$$
--- sdb_grid_viewer.py
+++ sdb_grid_viewer.py
@@ -225,12 +225,44 @@ tab_custom_plot = html.Div([
         justify='center'),
 ])
 
+tab_about = html.Div([
+    dcc.Markdown('''
+        # sdBGridViever
+
+        ***
+        
+        ## Overview
+        The tool to quickly inspect the grid of evolutionary models of sdB stars
+        calculated for the [ARDASTELLA](https://ardastella.up.krakow.pl/) research
+        group.
+        
+        The models were calculated using the set-up described
+        by [Ostrowski et al. (2021)](https://ui.adsabs.harvard.edu/abs/2021MNRAS.503.4646O/abstract)
+        , using the computational resources provided
+        by [Wrocław Centre for Networking and Supercomputing](https://www.wcss.pl/en/).
+        Evolutionary models were calculated using the
+        [MESA](https://github.com/MESAHub/mesa) code
+        ([Paxton et al. 2011](https://ui.adsabs.harvard.edu/abs/2011ApJS..192....3P/abstract),
+        [2013](https://ui.adsabs.harvard.edu/abs/2013ApJS..208....4P/abstract), 
+        [2015](https://ui.adsabs.harvard.edu/abs/2015ApJS..220...15P/abstract), 
+        [2018](https://ui.adsabs.harvard.edu/abs/2018ApJS..234...34P/abstract), 
+        [2019](https://ui.adsabs.harvard.edu/abs/2019ApJS..243...10P/abstract)),
+        version 11701, and supplemented by adiabatic non-radial pulsational models
+        calculated using the [GYRE](https://github.com/rhdtownsend/gyre) code
+        ([Townsend & Teitler 2013](https://ui.adsabs.harvard.edu/abs/2013MNRAS.435.3406T/abstract),
+        [Townsend et al. 2018](https://ui.adsabs.harvard.edu/abs/2018MNRAS.475..879T/abstract),
+        [Goldstein & Townsend 2020](https://ui.adsabs.harvard.edu/abs/2020ApJ...899..116G/abstract)),
+        version 5.2.
+    ''')
+])
+
 tabs = dbc.Tabs(
     [
         dbc.Tab(tab_logg_teff, label='logg vs. Teff'),
         dbc.Tab(tab_lum_teff, label='L vs. Teff'),
         dbc.Tab(tab_rad_teff, label='R vs. Teff'),
         dbc.Tab(tab_custom_plot, label='Custom plot'),
+        dbc.Tab(tab_about, label='About'),
     ]
 )
 
